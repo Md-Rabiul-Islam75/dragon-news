3ac0bff7accd51c36910e233fcee3a27451184ae
Md. Rabiul Islam <rabiul77777888@gmail.com>
2024-11-30 13:15:23 +0600

    design rightnavbar, use navlink on categorybtn and understand data architecture

---
+++ src/Components/FindUs.jsx
@@ -0,0 +1,20 @@
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+
+const FindUs = () => {
+  return (
+    <div>
+      <h2 className="font-semibold mb-3">Find Us On</h2>
+      <div className="join flex join-vertical *: bg-base-100">
+        <button className="btn join-item justify-start"><FaFacebook></FaFacebook>Facebook</button>
+
+        <button className="btn join-item justify-start"><FaInstagram></FaInstagram> Instagram</button>
+
+        <button className="btn join-item justify-start"><FaFacebook></FaFacebook>Facebook</button>
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default FindUs;
--- src/Components/layout-component/LeftNavbar.jsx
+++ src/Components/layout-component/LeftNavbar.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const LeftNavbar = () => {
     const [categories, setCategories] = useState([]);
@@ -15,10 +16,11 @@ const LeftNavbar = () => {
             <h2 className="font-semibold mb-3">All Category ({categories.length})</h2>
             <div className="flex flex-col gap-3">
                   {
-                    categories.map((category) => <button
+                    categories.map((category) => <NavLink
+                    to={`/category/${category.category_id}`}
                      className="btn bg-base-100 border-none"
                      key={category.category_id}>{category.category_name}
-                     </button>)
+                     </NavLink>)
                   }
             </div>
         </div>
